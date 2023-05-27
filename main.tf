fd6632dd2b3ca8d61c7dc62ae5e2200ffc4eeb93
Temmy-mike <engrolorunfemiajayi@gmail.com>
2023-05-27 03:52:40 +0100

    update count

--- main.tf
+++ main.tf
@@ -1,10 +1,13 @@
+
+
 resource "aws_instance" "cba_tf_instance" {
   instance_type = var.instance_type
   security_groups = [aws_security_group.cba_tf_sg.name]
   ami = var.instance_ami
   key_name = var.key_name
-  
+  count = 3
 
   tags = {
     Name = "CBATerraformInstance"
     }
+}
